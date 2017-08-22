c79ac213ae2741caaf27b529566e77faf8041f21
Shubat Max <shubat.max@gmail.com>
2017-08-22 19:27:51 +0300

    Source raw files to be unfolded

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// With this file we can change the order of loading scripts
+
+/*
+ * Third party JS
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+
+
+/*
+ * Custom JS
+ */
+//= particles/common.js
